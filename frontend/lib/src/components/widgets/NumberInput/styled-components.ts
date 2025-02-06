e7cb3dc7270a8f50d4084fc47ad17a5e0d8d4196
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-02-06 11:46:20 +0100

    Fix number input rounding and border (#10344)
    ## Describe your changes
    
    Applies two small fixes to number input so that it correctly handles the
    roundness from theme & that it uses the correct size if the border is
    not activated. Currently, instead of showing the background color as a
    border if not configured, it should use the secondary background color
    instead so that the border looks like it is part of the input field.
    
    ## Testing Plan
    
    - Added number input to theming e2e tests.
    
    ---
    
    **Contribution License Agreement**
    
    By submitting this pull request you agree that all contributions to this
    project are made under the Apache 2.0 license.

--- frontend/lib/src/components/widgets/NumberInput/styled-components.ts
+++ frontend/lib/src/components/widgets/NumberInput/styled-components.ts
@@ -26,8 +26,7 @@ export const StyledInputContainer = styled.div(({ theme }) => ({
   // to the entire container and not only the input itself
   borderWidth: theme.sizes.borderWidth,
   borderStyle: "solid",
-  // TODO(lukasmasuch): fix this to use secondaryBg instead of bgColor here:
-  borderColor: theme.colors.widgetBorderColor ?? theme.colors.bgColor,
+  borderColor: theme.colors.widgetBorderColor ?? theme.colors.secondaryBg,
   transitionDuration: "200ms",
   transitionProperty: "border",
   transitionTimingFunction: "cubic-bezier(0.2, 0.8, 0.4, 1)",

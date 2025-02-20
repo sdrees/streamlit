sdrees/streamlit

commit 2928ca995792a511d155354e7fdd2249534f1a13

Allow reusing existing event loop for server (#10164)
## MINOR CHANGES (1)
- Updated the logic of starting the server to prevent calling
`asyncio.run()` from a running event loop.

## Describe your changes
Refactored the server startup logic to ensure compatibility with both
existing and new event loops. This prevents the `RuntimeError`
encountered when `asyncio.run()` is called from a running event loop on
Python 3.10.16,

## GitHub Issue Link (if applicable)
No GitHub issue link available.

## Testing Plan
I would be grateful if someone could test the code using the existing
tests. The changes have been verified through manual testing.

- Explanation of why no additional tests are needed:
  The refactored code has been manually tested and works as expected.

- Unit Tests (JS and/or Python):
[@raethlein]: Added a Python unit test to ensure that `bootstrap.run`
works with and without an existing asyncio eventloop.

- E2E Tests:
  _None._

- Any manual testing needed?
Manual testing was performed to ensure the server starts correctly in
both new and existing event loops.

---


**Contribution License Agreement**

By submitting this pull request you agree that all contributions to this
project are made under the Apache 2.0 license.

---------

Co-authored-by: raethlein <benjamin.raethlein@gmail.com>

lib/streamlit/web/bootstrap.py
@@ -298,6 +298,8 @@ def run(
     is_hello: bool,
     args: list[str],
     flag_options: dict[str, Any],
+    *,
+    stop_immediately_for_testing: bool = False,
 ) -> None:
     """Run a script in a separate thread and start a server for the app.
 
@@ -321,9 +323,27 @@ def run(
         # and close all our threads
         _set_up_signal_handler(server)
 
+        # return immediately if we're testing the server start
+        if stop_immediately_for_testing:
+            _LOGGER.debug("Stopping server immediately for testing")
+            server.stop()
+
         # Wait until `Server.stop` is called, either by our signal handler, or
         # by a debug websocket session.
         await server.stopped
 
     # Run the server. This function will not return until the server is shut down.
-    asyncio.run(run_server())
+    # FIX RuntimeError: asyncio.run() cannot be called from a running event loop on Python 3.10.16
+    # asyncio.run(run_server())
+
+    # Define a main function to handle the event loop logic
+    async def main():
+        await run_server()
+
+    # Check if we're already in an event loop
+    if asyncio.get_event_loop().is_running():
+        # Use `asyncio.create_task` if we're in an async context
+        asyncio.create_task(main())
+    else:
+        # Otherwise, use `asyncio.run`
+        asyncio.run(main())

lib/tests/streamlit/web/bootstrap_test.py
@@ -21,6 +21,7 @@ from unittest import IsolatedAsyncioTestCase
 from unittest.mock import Mock, patch
 
 from streamlit import config
+from streamlit.runtime.runtime import Runtime
 from streamlit.web import bootstrap
 from tests import testutil
 from tests.testutil import patch_config_options
@@ -406,3 +407,26 @@ class BootstrapPrintTest(IsolatedAsyncioTestCase):
                 "server.port": 8502,
             },
         )
+
+
+class BootstrapRunTest(IsolatedAsyncioTestCase):
+    def tearDown(self):
+        #  Reset the Runtime._instance for subsequent test runs. Otherwise we will get
+        # a "Runtime already exists" error.
+        Runtime._instance = None
+
+    def test_bootstrap_run(self):
+        with testutil.patch_config_options({"server.headless": True}):
+            bootstrap.run("", False, [], {}, stop_immediately_for_testing=True)
+
+    def test_bootstrap_run_in_existing_event_loop(self):
+        import asyncio
+
+        event_loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(event_loop)
+        with testutil.patch_config_options({"server.headless": True}):
+
+            async def _run():
+                bootstrap.run("", False, [], {}, stop_immediately_for_testing=True)
+
+            event_loop.run_until_complete(_run())

commit 68f9db9721deb8a0f2d362d539a617aae12a60e6

[Fix] Reuse event loop logic (#10455)
`asyncio.get_running_loop()` throws a `RuntimeError: There is no current event loop in thread 'MainThread'` when this is no current event loop.
Resolves issue introduced in PR #10164. Closes #10452

lib/streamlit/web/bootstrap.py
@@ -340,10 +340,14 @@ def run(
     async def main():
         await run_server()
 
-    # Check if we're already in an event loop
-    if asyncio.get_event_loop().is_running():
-        # Use `asyncio.create_task` if we're in an async context
-        asyncio.create_task(main())
-    else:
-        # Otherwise, use `asyncio.run`
+    try:
+        # Check if we're already in an event loop
+        if asyncio.get_running_loop().is_running():
+            # Use `asyncio.create_task` if we're in an async context
+            asyncio.create_task(main())
+        else:
+            # Otherwise, use `asyncio.run`
+            asyncio.run(main())
+    except RuntimeError:
+        # get_running_loop throws RuntimeError if no running event loop
         asyncio.run(main())

lib/tests/streamlit/web/bootstrap_test.py
@@ -441,3 +441,12 @@ class BootstrapRunTest(IsolatedAsyncioTestCase):
                 bootstrap.run("", False, [], {}, stop_immediately_for_testing=True)
 
             event_loop.run_until_complete(_run())
+
+    def test_bootstrap_run_without_existing_event_loop(self):
+        import asyncio
+
+        # Remove the existing event loop
+        asyncio.set_event_loop(None)
+
+        with testutil.patch_config_options({"server.headless": True}):
+            bootstrap.run("", False, [], {}, stop_immediately_for_testing=True)